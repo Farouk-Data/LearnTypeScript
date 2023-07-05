38cab308b7a988b66afed67ff4385f73a64176a0
Farouk Kernel <farouk.echcharef@usmba.ac.ma>
2023-07-05 15:48:01 +0100

    learning tuples in TS (skipped some parts ill check out after)

---
+++ src/tuples.js
@@ -0,0 +1,3 @@
+//exemple: order of an api call
+var user;
+user = ["str", 69, true];
---
+++ src/tuples.ts
@@ -0,0 +1,6 @@
+//exemple: order of an api call
+
+let myUser: [string, number, boolean];
+
+//order of array types matter 
+myUser = ["str", 69, true];
